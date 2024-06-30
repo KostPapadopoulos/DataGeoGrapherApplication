be882beed5944b4906d33bcf19aec99e609327b9
BouzampalidisVasileiosIoannis <92268887+BouzampalidisVasileiosIoannis@users.noreply.github.com>
2024-06-30 20:07:41 +0300

    Update Homepage.tsx

--- React/datageographerfront/src/Homepage.tsx
+++ React/datageographerfront/src/Homepage.tsx
@@ -1,10 +1,9 @@
-import "./Homepage.css"; // Import your CSS file
+import "./Homepage.css"; 
 import React from 'react';
 import GoToFilters from "./GoToFilters";
 
 const Homepage: React.FC = () => {
   const handleButtonClick = () => {
-    // Handling navigation or other logic
     window.location.href = 'http://localhost:5173/filter-selection';
   };
 
